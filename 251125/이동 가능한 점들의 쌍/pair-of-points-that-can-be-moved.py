103f28d0a06a2fcbcb69b28f230025367e9c169c
14-2-kihyeon <tjrlgus19@gmail.com>
2025-11-25 21:02:55 +0900

    Title: 이동 가능한 점들의 쌍, Time: 364ms, Memory: 24MB, Status: Passed - Codetree

--- 251125/이동 가능한 점들의 쌍/pair-of-points-that-can-be-moved.py
+++ 251125/이동 가능한 점들의 쌍/pair-of-points-that-can-be-moved.py
@@ -1,47 +1,34 @@
 n, m, p, q = map(int, input().split())
 
-dist = [[[float('inf'), float('inf')] for _ in range(n+1)] for _ in range(n+1)]
+dist = [[float('inf')] * (n+1) for _ in range(n+1)]
+
+for _ in range(m):
+    n1, n2, w = map(int, input().split())
+    dist[n1][n2] = w
 
 for i in range(1, n+1):
-    dist[i][i][0] = 0
-    if i <= p:
-        dist[i][i][1] = 0
-
-
-for i in range(m):
-    n1, n2, w = map(int, input().split())
-    if n1 <= p or n2 <= p:
-        dist[n1][n2][1] = w
-    else:
-        dist[n1][n2][0] = w
-
+    dist[i][i] = 0
 
 for k in range(1, n+1):
     for i in range(1, n+1):
         for j in range(1, n+1):
-
-            nxt1 = dist[i][k][0] + dist[k][j][0]
-            if dist[i][j][0] > nxt1:
-                dist[i][j][0] = nxt1
-            nxt2 = min(
-                dist[i][k][1] + dist[k][j][0],
-                dist[i][k][0] + dist[k][j][1],
-                dist[i][k][1] + dist[k][j][1])
-            if dist[i][j][1] > nxt2:
-                dist[i][j][1] = nxt2
-
+            dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
 
 cnt = 0
 distance = 0
 
-for i in range(q):
+for _ in range(q):
     a, b = map(int, input().split())
 
-    if dist[a][b][1] != float('inf'):
-        cnt += 1
-        distance += dist[a][b][1]
-
-
+    min_dis = float('inf')
+    for i in range(1, p+1):
+        min_dis = min(min_dis, dist[a][i]+dist[i][b])
+    
+    if min_dis == float('inf'):
+        continue
+    cnt += 1
+    distance += min_dis
 
 print(cnt)
 print(distance)
+    
