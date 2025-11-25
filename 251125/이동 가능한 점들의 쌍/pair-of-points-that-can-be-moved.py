ee011e2a687f36e2555ffa38670dd9c80517a24c
14-2-kihyeon <tjrlgus19@gmail.com>
2025-11-25 19:21:06 +0900

    Title: 이동 가능한 점들의 쌍, Time: 170ms, Memory: 21MB, Status: Wrong Answer - Codetree

---
+++ 251125/이동 가능한 점들의 쌍/pair-of-points-that-can-be-moved.py
@@ -0,0 +1,37 @@
+n, m, p, q = map(int, input().split())
+
+dist = [[[float('inf'), 0] for _ in range(n+1)] for _ in range(n+1)]
+
+for i in range(1, n+1):
+    dist[i][i][0] = 0
+    dist[i][i][1] = 1
+
+for i in range(m):
+    n1, n2, w = map(int, input().split())
+    if n1 <= p or n2 <= p:
+        dist[n1][n2][1] = 1
+    dist[n1][n2][0] = w
+
+
+for k in range(1, n+1):
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            if dist[i][j][0] > dist[i][k][0] + dist[k][j][0]:
+                dist[i][j][0] = dist[i][k][0] + dist[k][j][0]
+                if dist[i][k][1] == 1 or dist[k][j][1] == 1:
+                    dist[i][j][1] = 1
+
+cnt = 0
+distance = 0
+
+for i in range(q):
+    a, b = map(int, input().split())
+    if dist[a][b][1] == 1:
+        if dist[a][b][0] != float('inf'):
+            cnt += 1
+            distance += dist[a][b][0]
+
+
+
+print(cnt)
+print(distance)
