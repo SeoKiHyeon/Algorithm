af1aedc919c81ea70be54792799b50072df8c5ed
14-2-kihyeon <tjrlgus19@gmail.com>
2025-12-02 21:15:13 +0900

    Title: 알바로 부자 되기, Time: 79ms, Memory: 17MB, Status: Passed - Codetree

--- 251202/알바로 부자 되기/being-rich-by-working-part-time.py
+++ 251202/알바로 부자 되기/being-rich-by-working-part-time.py
@@ -1,16 +1,25 @@
-n = int(input())
-jobs = [tuple(map(int, input().split())) for _ in range(n)]
 
-jobs.sort()
+N = int(input().strip())
+jobs = []
 
-dp = [0] * (n+1)
-dp[0] = jobs[0][2]
+for _ in range(N):
+    s, e, p = map(int, input().split())
+    jobs.append((s, e, p))
 
-for i in range(1, n):
-    dp[i] = jobs[i][2]
+# 끝나는 날 기준으로 정렬
+jobs.sort(key=lambda x: x[1])
 
-    for j in range(i):
-        if jobs[i][0] > jobs[j][1]:
-            dp[i] = max(dp[i], dp[j]+jobs[j][2])
+# dp[i] : jobs[0..i-1] 까지 고려했을 때 얻을 수 있는 최대 수입
+dp = [0] * (N + 1)
 
-print(max(dp))
+for i in range(1, N + 1):
+    best_price = 0
+    for j in range(i-1, 0, -1):
+        if jobs[i-1][0] > jobs[j-1][1]:
+            best_price = dp[j]
+            break
+    
+    dp[i] = max(dp[i-1], best_price + jobs[i-1][2])
+
+
+print(dp[N])
