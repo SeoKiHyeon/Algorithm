1826fb5f505a0f559f9554f5e7ea83abaeccd36c
14-2-kihyeon <tjrlgus19@gmail.com>
2025-12-02 19:45:05 +0900

    Title: 알바로 부자 되기, Time: 49ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251202/알바로 부자 되기/being-rich-by-working-part-time.py
@@ -0,0 +1,16 @@
+n = int(input())
+jobs = [tuple(map(int, input().split())) for _ in range(n)]
+
+jobs.sort()
+
+dp = [0] * (n+1)
+dp[0] = jobs[0][2]
+
+for i in range(1, n):
+    dp[i] = jobs[i][2]
+
+    for j in range(i):
+        if jobs[i][0] > jobs[j][1]:
+            dp[i] = max(dp[i], dp[j]+jobs[j][2])
+
+print(max(dp))
