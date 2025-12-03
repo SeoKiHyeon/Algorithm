2e59e146c39d3c81afa1973a9edec1a71d45e0c1
14-2-kihyeon <tjrlgus19@gmail.com>
2025-12-03 17:45:21 +0900

    Title: 최대 동전 거슬러주기, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251203/최대 동전 거슬러주기/max-coin-change.py
+++ 251203/최대 동전 거슬러주기/max-coin-change.py
@@ -7,6 +7,7 @@ dp[0] = 0
 for i in range(1, m+1):
     for j in range(n):
         if i >= arr[j]:
+            if dp[i-arr[j]] == float('-inf'): continue 
             dp[i] = max(dp[i], dp[i-arr[j]] + 1)
 
 print(dp[m])
