3cd16e8dd43286ea113504c124ecf04149ab9c89
14-2-kihyeon <tjrlgus19@gmail.com>
2025-12-06 17:09:20 +0900

    Title: 최소 차 분할, Time: 61ms, Memory: 17MB, Status: Passed - Codetree

---
+++ 251206/최소 차 분할/minimum-diff-partition.py
@@ -0,0 +1,23 @@
+n = int(input())
+arr = [0] + list(map(int, input().split()))
+
+m = sum(arr)
+
+dp = [[0] * (m+1) for _ in range(n+1)]
+dp[0][0] = True
+
+for i in range(1, n+1):
+    for j in range(m+1):
+        if j >= arr[i] and dp[i-1][j-arr[i]]:
+            dp[i][j] = True
+        
+        if dp[i-1][j]:
+            dp[i][j] = True
+
+
+ans = float('inf')
+for i in range(1, m):
+    if dp[n][i]:
+        ans = min(ans, abs(i- (m-i)))
+
+print(ans)
